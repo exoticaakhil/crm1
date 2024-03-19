7cec90103674f1b0df6ebffacc1e77b52b16828c
exoticaakhil <akhilanilvly@gmail.com>
2024-03-19 10:20:04 +0530

    commit3

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('login', views.login, name='login'),
+    path('sign', views.sign, name='sign'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('archive', views.archive, name='archive'),
+    path('page1', views.page1, name='page1'),
+    path('page2', views.page2, name='page2'),
+
 ]
--- crmapp/views.py
+++ crmapp/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+def login(request):
+  return render(request, 'login.html')
+def sign(request):
+  return render(request, 'signup.html')
+def dashboard(request):
+    item_idf = request.GET.get('item_idf',' ').split(',')
+    return render(request, 'dashboard.html',{'item_idf':item_idf})
+def archive(request):
+    item_ids = request.GET.get('item_ids',' ').split(',')
+    return render(request, 'archive.html', {'item_ids': item_ids}) 
+
+def page1(request):
+    item_idf = request.GET.get('item_idf',' ').split(',')
+    return render(request, 'page1.html',{'item_idf':item_idf})
+
+def page2(request):
+    item_ids = request.GET.get('item_ids',' ').split(',')
+    return render(request, 'page2.html', {'item_ids': item_ids}) 
